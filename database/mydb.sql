716b78e526274b21bd7ac56658181579a8d148e7
MasDeny <kickdeny@gmail.com>
2017-12-18 04:08:14 +0700

    commit #60
    database

--- database/mydb.sql
+++ database/mydb.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 09, 2017 at 03:38 PM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 7.0.9
+-- Host: localhost
+-- Generation Time: Dec 17, 2017 at 08:48 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -34,7 +36,8 @@ CREATE TABLE `berkas` (
   `khs_semester_4` varchar(100) DEFAULT NULL,
   `khs_semester_5` varchar(100) DEFAULT NULL,
   `laporan_proposal` varchar(100) DEFAULT NULL,
-  `lembar_revisi` varchar(100) DEFAULT NULL
+  `kartu_kendali` varchar(100) DEFAULT NULL,
+  `bukti_spp_terakhir` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -47,7 +50,6 @@ CREATE TABLE `berkasta` (
   `idBerkasTA` int(11) NOT NULL,
   `beritaAcaraTA` varchar(45) DEFAULT NULL,
   `sampul` varchar(45) DEFAULT NULL,
-  `judul` varchar(45) DEFAULT NULL,
   `pengesahanTA` varchar(45) DEFAULT NULL,
   `persembahan` varchar(45) DEFAULT NULL,
   `motto` varchar(45) DEFAULT NULL,
@@ -68,10 +70,7 @@ CREATE TABLE `berkasta` (
   `lampiran` varchar(45) DEFAULT NULL,
   `statusBerkasTA` tinyint(1) DEFAULT NULL,
   `dibuat` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `mahasiswa_NIM` varchar(9) NOT NULL,
-  `mahasiswa_id_mhs` int(11) NOT NULL,
-  `mahasiswa_id_dokumen` int(11) NOT NULL,
-  `login_karyawan_id_karyawan` int(11) NOT NULL
+  `NIM` varchar(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,13 +89,6 @@ CREATE TABLE `berkas_khs` (
   `khs_semester6` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `berkas_khs`
---
-
-INSERT INTO `berkas_khs` (`idberkas_khs`, `khs_semester1`, `khs_semester2`, `khs_semester3`, `khs_semester4`, `khs_semester5`, `khs_semester6`) VALUES
-('BKS-001', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH13.', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH41.', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH42.', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH61.', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH62.', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH81.');
-
 -- --------------------------------------------------------
 
 --
@@ -111,13 +103,6 @@ CREATE TABLE `berkas_pkl` (
   `bukti_laporanpkl` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `berkas_pkl`
---
-
-INSERT INTO `berkas_pkl` (`idberkas_pkl`, `judul_pkl`, `nilai_pkl`, `berita_acarapkl`, `bukti_laporanpkl`) VALUES
-('PKL-001', 'fd', '90', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH82.', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH91.');
-
 -- --------------------------------------------------------
 
 --
@@ -130,24 +115,6 @@ CREATE TABLE `berkas_ta` (
   `nilai_TA` varchar(45) DEFAULT NULL,
   `berita_acaraTA` varchar(45) DEFAULT NULL,
   `bukti_laporanTA` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `berkas_ta`
---
-
-INSERT INTO `berkas_ta` (`idberkas_TA`, `judul_TA`, `nilai_TA`, `berita_acaraTA`, `bukti_laporanTA`) VALUES
-('BTA-001', 'fd', '44', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH111', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH101');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bukti_spp_terakhir`
---
-
-CREATE TABLE `bukti_spp_terakhir` (
-  `id_bukti` int(11) NOT NULL,
-  `berkas_spp` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -183,14 +150,6 @@ CREATE TABLE `dokumen_tugas_akhir` (
   `verifikasi_tugas_akhir` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `dokumen_tugas_akhir`
---
-
-INSERT INTO `dokumen_tugas_akhir` (`id_dokumen`, `berkas_dokumen`, `verifikasi_tugas_akhir`) VALUES
-(1, 'fda', 'fda'),
-(2, 'fda', 'fdas');
-
 -- --------------------------------------------------------
 
 --
@@ -207,8 +166,9 @@ CREATE TABLE `dp1` (
 --
 
 INSERT INTO `dp1` (`id_dp1`, `nm_dosen`) VALUES
-(1, 'fdsaf'),
-(2, 'fdafa');
+(1, 'rudi setiawan'),
+(2, 'Heru Santoso'),
+(3, 'Edi Sumanto');
 
 -- --------------------------------------------------------
 
@@ -224,8 +184,7 @@ CREATE TABLE `jadwal` (
   `panelis_1` varchar(45) DEFAULT NULL,
   `panelis_2` varchar(45) DEFAULT NULL,
   `panelis_3` varchar(45) DEFAULT NULL,
-  `koordinator_NIP` int(11) DEFAULT NULL,
-  `Status_Sempro_idStatus_Sempro` int(11) NOT NULL
+  `idStatus_Sempro` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -246,7 +205,8 @@ CREATE TABLE `login_karyawan` (
 --
 
 INSERT INTO `login_karyawan` (`id_karyawan`, `NIK`, `password`, `status`) VALUES
-(1, '110110', '110110', 'admin');
+(1, '12354', 'taufik', 'koordinator'),
+(2, '12345', 'rini', 'admin');
 
 -- --------------------------------------------------------
 
@@ -265,9 +225,10 @@ CREATE TABLE `login_mhs` (
 --
 
 INSERT INTO `login_mhs` (`id_mhs`, `NIM`, `password`) VALUES
-(1, 'e31150089', 'e31150089'),
-(2, 'e31150025', 'e31150025'),
-(3, 'E31150088', 'user');
+(1, 'E31150025', 'E31150025'),
+(2, 'E31150089', 'E31150089'),
+(3, 'E31150198', 'E31150198'),
+(4, 'E31150678', 'E31150678');
 
 -- --------------------------------------------------------
 
@@ -285,19 +246,26 @@ CREATE TABLE `mahasiswa` (
   `judul_TA` varchar(120) DEFAULT NULL,
   `deskripsi` varchar(200) DEFAULT NULL,
   `id_mhs` int(11) NOT NULL,
-  `id_dokumen` int(11) NOT NULL DEFAULT '0',
-  `status` varchar(20) NOT NULL,
-  `status_mahasiswa` varchar(45) DEFAULT NULL
+  `id_dokumen` int(11) NOT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `status_mahasiswa` varchar(45) DEFAULT NULL,
+  `keterangan` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `mahasiswa`
+-- Table structure for table `nilai_sidang`
 --
 
-INSERT INTO `mahasiswa` (`NIM`, `nama_mahasiswa`, `program_studi`, `id_dp1`, `dospem_2`, `id_rumpun`, `judul_TA`, `deskripsi`, `id_mhs`, `id_dokumen`, `status`, `status_mahasiswa`) VALUES
-('e31150025', 'askdjask', 'jaksdjaksdj', 1, 'jaskdjaskd', 1, 'jaksdjaskdj', 'ajksdjkdj', 2, 1, '1', '1'),
-('E31150088', 'saldks', 'adlkasld', 1, 'asdj', 1, 'asd', 'dsdsd', 3, 1, '1', '1'),
-('e31150089', 'dedi kahfi', 'mif', 1, 'fda', 1, 'dfas', 'fdada', 1, 1, '2', 'alumni');
+CREATE TABLE `nilai_sidang` (
+  `idnilai_sidang` int(11) NOT NULL,
+  `nilai_bimbingan_dp1` varchar(10) DEFAULT NULL,
+  `nilai_bimbingan_dp2` varchar(10) DEFAULT NULL,
+  `nilai_ujian` varchar(10) DEFAULT NULL,
+  `nilai_total` varchar(10) DEFAULT NULL,
+  `status_sidang_id_statussidang` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -346,13 +314,6 @@ CREATE TABLE `riwayat_ipmhs` (
   `ipk` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `riwayat_ipmhs`
---
-
-INSERT INTO `riwayat_ipmhs` (`idriwayat_ipmhs`, `ipsemester1`, `ipsemester2`, `ipsemester3`, `ipsemester4`, `ipsemester5`, `ipsemester6`, `ipk`) VALUES
-('KHS-001', '3', '3', '3', '3', '3', '3', '3');
-
 -- --------------------------------------------------------
 
 --
@@ -369,8 +330,9 @@ CREATE TABLE `rumpun` (
 --
 
 INSERT INTO `rumpun` (`id_rumpun`, `nm_rumpun`) VALUES
-(1, 'fdas'),
-(2, 'dfafad');
+(1, 'web'),
+(2, 'android'),
+(3, 'desktop');
 
 -- --------------------------------------------------------
 
@@ -382,7 +344,9 @@ CREATE TABLE `status_sempro` (
   `idStatus_Sempro` int(11) NOT NULL,
   `mahasiswa_NIM` varchar(9) NOT NULL,
   `dokumen_kelengkapan_senpro_iddokumen_kelengkapan_senpro` int(11) NOT NULL,
-  `status` int(11) DEFAULT '0'
+  `status` int(11) DEFAULT '0',
+  `bukti_revisi_sempro` varchar(200) DEFAULT NULL,
+  `status_revisi` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -436,20 +400,12 @@ CREATE TABLE `yudisium` (
   `angkatan` varchar(45) DEFAULT NULL,
   `srt_bebastanggungan` varchar(45) DEFAULT NULL,
   `konsep_daftarnilai` varchar(45) DEFAULT NULL,
-  `status` varchar(20) NOT NULL,
   `mahasiswa_NIM` varchar(9) NOT NULL,
   `berkas_TA_idberkas_TA` char(10) NOT NULL,
   `berkas_pkl_idberkas_pkl` char(10) NOT NULL,
   `riwayat_ipmhs_idriwayat_ipmhs` char(10) NOT NULL,
   `berkas_khs_idberkas_khs` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `yudisium`
---
-
-INSERT INTO `yudisium` (`idyudisium`, `angkatan`, `srt_bebastanggungan`, `konsep_daftarnilai`, `status`, `mahasiswa_NIM`, `berkas_TA_idberkas_TA`, `berkas_pkl_idberkas_pkl`, `riwayat_ipmhs_idriwayat_ipmhs`, `berkas_khs_idberkas_khs`) VALUES
-('STD-001', '2015', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH102', 'DieCutting_SIMPANG_TIGA_16_April_2016_PATH83.', '', 'e31150089', 'BTA-001', 'PKL-001', 'KHS-001', 'BKS-001');
 
 --
 -- Indexes for dumped tables
@@ -460,14 +416,6 @@ INSERT INTO `yudisium` (`idyudisium`, `angkatan`, `srt_bebastanggungan`, `konsep
 --
 ALTER TABLE `berkas`
   ADD PRIMARY KEY (`id_berkas_kelengkapan`);
-
---
--- Indexes for table `berkasta`
---
-ALTER TABLE `berkasta`
-  ADD PRIMARY KEY (`idBerkasTA`,`mahasiswa_NIM`,`mahasiswa_id_mhs`,`mahasiswa_id_dokumen`,`login_karyawan_id_karyawan`),
-  ADD KEY `fk_berkasTA_mahasiswa1_idx` (`mahasiswa_NIM`,`mahasiswa_id_mhs`,`mahasiswa_id_dokumen`),
-  ADD KEY `fk_berkasTA_login_karyawan1_idx` (`login_karyawan_id_karyawan`);
 
 --
 -- Indexes for table `berkas_khs`
@@ -486,12 +434,6 @@ ALTER TABLE `berkas_pkl`
 --
 ALTER TABLE `berkas_ta`
   ADD PRIMARY KEY (`idberkas_TA`);
-
---
--- Indexes for table `bukti_spp_terakhir`
---
-ALTER TABLE `bukti_spp_terakhir`
-  ADD PRIMARY KEY (`id_bukti`);
 
 --
 -- Indexes for table `dokumen_kelengkapan_senpro`
@@ -515,8 +457,8 @@ ALTER TABLE `dp1`
 -- Indexes for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  ADD PRIMARY KEY (`idJadwal`,`Status_Sempro_idStatus_Sempro`),
-  ADD KEY `fk_Jadwal_Status_Sempro1_idx` (`Status_Sempro_idStatus_Sempro`);
+  ADD PRIMARY KEY (`idJadwal`,`idStatus_Sempro`),
+  ADD KEY `fk_Jadwal_Status_Sempro1_idx` (`idStatus_Sempro`);
 
 --
 -- Indexes for table `login_karyawan`
@@ -534,12 +476,18 @@ ALTER TABLE `login_mhs`
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`NIM`,`id_mhs`,`id_dokumen`,`id_rumpun`) USING BTREE,
+  ADD PRIMARY KEY (`NIM`,`id_dp1`,`id_rumpun`,`id_mhs`,`id_dokumen`),
   ADD KEY `fk_mahasiswa_login_mhs1_idx` (`id_mhs`),
   ADD KEY `fk_mahasiswa_dokumen_tugas_akhir1_idx` (`id_dokumen`),
   ADD KEY `fk_mahasiswa_rumpun1_idx` (`id_rumpun`),
-  ADD KEY `fk_mahasiswa_dp11_idx` (`id_dp1`),
-  ADD KEY `id_rumpun` (`id_rumpun`);
+  ADD KEY `fk_mahasiswa_dp11_idx` (`id_dp1`);
+
+--
+-- Indexes for table `nilai_sidang`
+--
+ALTER TABLE `nilai_sidang`
+  ADD PRIMARY KEY (`idnilai_sidang`,`status_sidang_id_statussidang`),
+  ADD KEY `fk_nilai_sidang_status_sidang1_idx` (`status_sidang_id_statussidang`);
 
 --
 -- Indexes for table `notification`
@@ -552,7 +500,7 @@ ALTER TABLE `notification`
 -- Indexes for table `penguji`
 --
 ALTER TABLE `penguji`
-  ADD PRIMARY KEY (`id_penguji`,`status_sidang_id_statussidang`),
+  ADD PRIMARY KEY (`id_penguji`,`status_sidang_id_statussidang`) USING BTREE,
   ADD KEY `fk_penguji_status_sidang1_idx` (`status_sidang_id_statussidang`);
 
 --
@@ -580,7 +528,6 @@ ALTER TABLE `status_sempro`
 --
 ALTER TABLE `status_sidang`
   ADD PRIMARY KEY (`id_statussidang`,`bukti_spp_terakhir_id_bukti`,`mahasiswa_NIM`,`berkas_id_berkas_kelengkapan`),
-  ADD KEY `fk_status_sidang_bukti_spp_terakhir1_idx` (`bukti_spp_terakhir_id_bukti`),
   ADD KEY `fk_status_sidang_mahasiswa1_idx` (`mahasiswa_NIM`),
   ADD KEY `fk_status_sidang_berkas1_idx` (`berkas_id_berkas_kelengkapan`);
 
@@ -613,40 +560,40 @@ ALTER TABLE `yudisium`
 ALTER TABLE `berkas`
   MODIFY `id_berkas_kelengkapan` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `bukti_spp_terakhir`
---
-ALTER TABLE `bukti_spp_terakhir`
-  MODIFY `id_bukti` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `dokumen_kelengkapan_senpro`
 --
 ALTER TABLE `dokumen_kelengkapan_senpro`
-  MODIFY `iddokumen_kelengkapan_senpro` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `iddokumen_kelengkapan_senpro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT for table `dokumen_tugas_akhir`
 --
 ALTER TABLE `dokumen_tugas_akhir`
-  MODIFY `id_dokumen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_dokumen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `dp1`
 --
 ALTER TABLE `dp1`
-  MODIFY `id_dp1` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_dp1` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `idJadwal` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idJadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `login_karyawan`
 --
 ALTER TABLE `login_karyawan`
-  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `login_mhs`
 --
 ALTER TABLE `login_mhs`
-  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `nilai_sidang`
+--
+ALTER TABLE `nilai_sidang`
+  MODIFY `idnilai_sidang` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `notification`
 --
@@ -661,12 +608,12 @@ ALTER TABLE `penguji`
 -- AUTO_INCREMENT for table `rumpun`
 --
 ALTER TABLE `rumpun`
-  MODIFY `id_rumpun` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_rumpun` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `status_sempro`
 --
 ALTER TABLE `status_sempro`
-  MODIFY `idStatus_Sempro` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idStatus_Sempro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `status_sidang`
 --
@@ -677,17 +624,25 @@ ALTER TABLE `status_sidang`
 --
 
 --
--- Constraints for table `berkasta`
---
-ALTER TABLE `berkasta`
-  ADD CONSTRAINT `fk_berkasTA_login_karyawan1` FOREIGN KEY (`login_karyawan_id_karyawan`) REFERENCES `login_karyawan` (`id_karyawan`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_berkasTA_mahasiswa1` FOREIGN KEY (`mahasiswa_NIM`,`mahasiswa_id_mhs`,`mahasiswa_id_dokumen`) REFERENCES `mahasiswa` (`NIM`, `id_mhs`, `id_dokumen`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
 -- Constraints for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  ADD CONSTRAINT `fk_Jadwal_Status_Sempro1` FOREIGN KEY (`Status_Sempro_idStatus_Sempro`) REFERENCES `status_sempro` (`idStatus_Sempro`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Jadwal_Status_Sempro1` FOREIGN KEY (`idStatus_Sempro`) REFERENCES `status_sempro` (`idStatus_Sempro`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD CONSTRAINT `fk_mahasiswa_dokumen_tugas_akhir1` FOREIGN KEY (`id_dokumen`) REFERENCES `dokumen_tugas_akhir` (`id_dokumen`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_mahasiswa_dp11` FOREIGN KEY (`id_dp1`) REFERENCES `dp1` (`id_dp1`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_mahasiswa_login_mhs1` FOREIGN KEY (`id_mhs`) REFERENCES `login_mhs` (`id_mhs`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_mahasiswa_rumpun1` FOREIGN KEY (`id_rumpun`) REFERENCES `rumpun` (`id_rumpun`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `nilai_sidang`
+--
+ALTER TABLE `nilai_sidang`
+  ADD CONSTRAINT `fk_nilai_sidang_status_sidang1` FOREIGN KEY (`status_sidang_id_statussidang`) REFERENCES `status_sidang` (`id_statussidang`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `notification`
@@ -702,18 +657,10 @@ ALTER TABLE `penguji`
   ADD CONSTRAINT `fk_penguji_status_sidang1` FOREIGN KEY (`status_sidang_id_statussidang`) REFERENCES `status_sidang` (`id_statussidang`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `status_sempro`
---
-ALTER TABLE `status_sempro`
-  ADD CONSTRAINT `fk_Status_Sempro_dokumen_kelengkapan_senpro1` FOREIGN KEY (`dokumen_kelengkapan_senpro_iddokumen_kelengkapan_senpro`) REFERENCES `dokumen_kelengkapan_senpro` (`iddokumen_kelengkapan_senpro`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Status_Sempro_mahasiswa1` FOREIGN KEY (`mahasiswa_NIM`) REFERENCES `mahasiswa` (`NIM`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
 -- Constraints for table `status_sidang`
 --
 ALTER TABLE `status_sidang`
   ADD CONSTRAINT `fk_status_sidang_berkas1` FOREIGN KEY (`berkas_id_berkas_kelengkapan`) REFERENCES `berkas` (`id_berkas_kelengkapan`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_status_sidang_bukti_spp_terakhir1` FOREIGN KEY (`bukti_spp_terakhir_id_bukti`) REFERENCES `bukti_spp_terakhir` (`id_bukti`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_status_sidang_mahasiswa1` FOREIGN KEY (`mahasiswa_NIM`) REFERENCES `mahasiswa` (`NIM`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
@@ -731,6 +678,7 @@ ALTER TABLE `yudisium`
   ADD CONSTRAINT `fk_yudisium_berkas_pkl1` FOREIGN KEY (`berkas_pkl_idberkas_pkl`) REFERENCES `berkas_pkl` (`idberkas_pkl`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_yudisium_mahasiswa1` FOREIGN KEY (`mahasiswa_NIM`) REFERENCES `mahasiswa` (`NIM`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_yudisium_riwayat_ipmhs1` FOREIGN KEY (`riwayat_ipmhs_idriwayat_ipmhs`) REFERENCES `riwayat_ipmhs` (`idriwayat_ipmhs`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
